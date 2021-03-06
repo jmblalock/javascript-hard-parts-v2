834cdbdd94a5b682ded5b587dbdda874e5bd7f2a
jmblalock <justin.m.blalock@gmail.com>
2021-03-05 18:45:29 -0600

    retaining-function-memory

---
+++ closure/retaining-function-memory.js
@@ -0,0 +1,13 @@
+// Calling a function outside of the function call in which it was defined
+function outer() {
+  let counter = 0;
+  function incrementCounter() {
+    counter++;
+    // console.log(counter);
+  }
+  return incrementCounter;
+}
+
+const myNewFunction = outer();
+myNewFunction();
+myNewFunction();
